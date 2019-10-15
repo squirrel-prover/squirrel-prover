51664d863df5b6d92eb49e15c77686e618290772
Charlie Jacomme <jacomme@crans.org>
2019-10-15 17:26:01 +0200

    clean up : action.ml

--- src/action.ml
+++ src/action.ml
@@ -1,54 +1,42 @@
 open Vars
-   
+
 module IndexParam : VarParam =
 struct
   let default_string = "index"
-  let cpt = ref 0 
+  let cpt = ref 0
 end
 
 module Index = Var(IndexParam)
 
 type index = Index.t
 
-(** In the process (A | Pi_i B(i) | C) actions of A have par_choice 0,
-  * actions of C have par_choice 2, and those of B have par_choice
-  * (1,i) which will later be instantiated to (1,i_1), (1,i_2), etc.
-  *
-  * Then, in a process (if cond then P else Q), the sum_choice 0 will
-  * denote a success of the conditional, while 1 will denote a failure. *)
 type 'a item = {
   par_choice : int * 'a ;
   sum_choice : int
 }
+
 type 'a t = 'a item list
 
-(** Checks whether two actions are in conflict. *)
-let rec conflict a b = match a,b with
+let rec conflict a b = match a, b with
   | hda::tla, hdb::tlb ->
     hda.par_choice = hdb.par_choice &&
     (hda.sum_choice <> hdb.sum_choice ||
      conflict tla tlb)
   | _ -> false
 
-(** [depends a b] test if [a] must occur before [b] as far
-  * as the control-flow is concerned -- it does not (cannot)
-  * take messages into account. *)
-let rec depends a b = match a,b with
-  | [],_ -> true
+let rec depends a b = match a, b with
+  | [], _ -> true
   | hda::tla, hdb::tlb ->
     hda = hdb &&
     depends tla tlb
   | _ -> false
 
-(** [enables a b] tests whether action [a] enables [b]. *)
-let rec enables a b = match a,b with
-  | [],[_] -> true
+let rec enables a b = match a, b with
+  | [], [_] -> true
   | hda::tla, hdb::tlb ->
     hda = hdb &&
     enables tla tlb
   | _ -> false
-
-
 
 type action_shape = int t
 
@@ -62,34 +50,32 @@ let rec get_shape = function
   | [] -> []
   | i :: l ->
     let p, sis = i.par_choice in
-    { par_choice = (p, List.length sis) ;
+    { par_choice = (p, List.length sis);
       sum_choice = i.sum_choice }
     :: get_shape l
 
 let rec action_indices = function
   | [] -> []
-  | a :: l -> let _,sis = a.par_choice in
+  | a :: l -> let _, sis = a.par_choice in
     sis @ action_indices l
 
 let same_shape a b =
   let rec same acc a b = match a,b with
-  | [],[] -> Some acc
-  | [], _ | _, [] -> None
-  | i :: l, i' :: l' ->
-    let p,sis = i.par_choice and p',sis' = i'.par_choice in    
-    if p = p' && List.length sis = List.length sis' then
-      let acc' = List.map2 (fun (i) (i') -> i,i') sis sis' in
-      same (acc' @ acc) l l'
-    else None in
-   same [] a b 
+    | [],[] -> Some acc
+    | [], _ | _, [] -> None
+    | i :: l, i' :: l' ->
+      let p,sis = i.par_choice and p', sis' = i'.par_choice in
+      if p = p' && List.length sis = List.length sis' then
+        let acc' = List.map2 (fun i i' -> i,i') sis sis' in
+        same (acc' @ acc) l l'
+      else None in
+  same [] a b
 
-(** [constr_equal a b] returns the list of index constraints necessary to have
-    [a] and [b] equal, if there is one. Return None otherwise. *)
 let rec constr_equal a b = match a,b with
   | [],[] -> Some []
   | [], _ | _, [] -> None
   | i :: l, i' :: l' ->
-    let _,sis = i.par_choice and _,sis' = i'.par_choice in
+    let _, sis = i.par_choice and _, sis' = i'.par_choice in
     Utils.opt_map
       (constr_equal a b)
       (fun res ->
@@ -99,24 +85,22 @@ let rec constr_equal a b = match a,b with
 
 let rec refresh = function
   | [] -> [],[]
-  | {par_choice=(k,is);sum_choice}::l ->
-      let l3 = List.map (fun (i) -> i, Index.make_fresh ()) is in
-      let is' = List.map (fun (i,j) -> j) l3 in
-      let newsubst = l3 in
-      let action,subst = refresh l in
-        { par_choice= k, is' ;
-          sum_choice }
-        :: action,
-        newsubst @ subst
+  | { par_choice=(k, is);
+      sum_choice}::l ->
+    let l3 = List.map (fun (i) -> i, Index.make_fresh ()) is in
+    let is' = List.map (fun (i, j) -> j) l3 in
+    let newsubst = l3 in
+    let action,subst = refresh l in
+    { par_choice= k, is' ;
+      sum_choice }
+    :: action,
+    newsubst @ subst
 
 let pp_par_choice_fg f g ppf (k,str_indices) =
   if str_indices = [] then
     Fmt.pf ppf "%d" k
   else
     Fmt.pf ppf "%d[%a]" k f (g str_indices)
-
-
-
 
 let pp_par_choice =
   pp_par_choice_fg Index.pp_list (fun sis -> sis)
@@ -135,25 +119,26 @@ let pp_par_choice_shape2 =
 let rec pp_action_f f ppf = function
   | [] -> Fmt.pf ppf ""
   | [{ par_choice; sum_choice }] ->
-      if sum_choice = 0 then
-        Fmt.pf ppf "%a"
-          f par_choice
-      else
-        Fmt.pf ppf "%a/%d"
-          f par_choice
-          sum_choice
+    if sum_choice = 0 then
+      Fmt.pf ppf "%a"
+        f par_choice
+    else
+      Fmt.pf ppf "%a/%d"
+        f par_choice
+        sum_choice
   | { par_choice; sum_choice } :: l ->
-      if sum_choice = 0 then
-        Fmt.pf ppf "%a_%a"
-          f par_choice
-          (pp_action_f f) l
-      else
-        Fmt.pf ppf "%a/%d_%a"
-          f par_choice
-          sum_choice
-          (pp_action_f f) l
+    if sum_choice = 0 then
+      Fmt.pf ppf "%a_%a"
+        f par_choice
+        (pp_action_f f) l
+    else
+      Fmt.pf ppf "%a/%d_%a"
+        f par_choice
+        sum_choice
+        (pp_action_f f) l
+
 let pp_action ppf a =
- Fmt.styled `Green (pp_action_f pp_par_choice) ppf a
+  Fmt.styled `Green (pp_action_f pp_par_choice) ppf a
 
 let pp_shape = pp_action_f pp_par_choice_shape
 
--- src/action.mli
+++ src/action.mli
@@ -1,32 +1,50 @@
 (** Processes are decomposed as structured sets of actions. *)
-
 open Vars
 
-(** Indices are used to generate arbitrary families of terms *)
+(** Indices are used to generate arbitrary families of terms, and thus duplicate
+    of actions *)
 module Index : VarType
 
 type index = Index.t
 
+(** In the process (A | Pi_i B(i) | C) actions of A have par_choice actions of C
+    have par_choice 2, and those of B have par_choice (1,i)which will later be
+    instantiated to (1,i_1), (1,i_2), etc. Then, in a process (if cond then P
+    else Q), the sum_choice 0 will * denote a success of   the conditional,
+    while 1 will denote a failure. *)
 type 'a item = {
   par_choice : int * 'a ;
   sum_choice : int
 }
+
+(** The position of an action inside a process can be captured by a list of
+    parallel and sum choices *)
 type 'a t = ('a item) list
 
+(** [conflict a b] checks whether two actions [a] and [b] are in conflict. *)
 val conflict : 'a t -> 'a t -> bool
 
+(** [depends a b] test if [a] must occur before [b] as far
+    as the control-flow is concerned -- it does not (cannot)
+    take messages into account. *)
 val depends : 'a t -> 'a t -> bool
 
+(** [enables a b] tests whether action [a] enables [b]. *)
 val enables : 'a t -> 'a t -> bool
 
+(** [action_shape] captures the position of an action inside a process, but not
+    the potential index variables upon which it depends. It only captures the
+    number of index variables defined at multiple places *)
 type action_shape = int t
 
+(** An [action] is an [action_shape] inside which all index variables are
+    explicits. *)
 type action = (index list) t
 
-val mk_shape : int t -> action_shape
-
+(** [get_shape a] extracts the action_shape of an action *)
 val get_shape : action -> action_shape
 
+(** [action_indices a] gives back all index appearing inside the action *)
 val action_indices : action -> index list
 
 (** [same_shape a b] returns [Some subst] if [a] and [b] have the same action
