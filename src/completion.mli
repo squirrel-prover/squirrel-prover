7282b77226e02edd4b57de47fa02b8291e35419c
Charlie Jacomme <jacomme@crans.org>
2019-10-15 17:51:17 +0200

    cleanup completion

--- src/completion.mli
+++ src/completion.mli
@@ -1,13 +1,19 @@
+(** Completion of equalities and disequalities set *)
 open Term
 
 type state
 
+(** [complete l] construct a complete term rewritting system from the set of
+    equations inside l *)
 val complete : (term * term) list -> state
 
+(** [check_disequalities s l] checks that all disequalities inside [l] holds
+    w.r.t [s] *)
 val check_disequalities : state -> (term * term) list -> bool
 
+(** [check_equalities s l] checks that all equalities inside [l] holds
+    w.r.t [s] *)
 val check_equalities : state -> (term * term) list -> bool
-
 
 (** [name_index_cnstrs state l] looks for all names that are equal w.r.t. the
     rewrite relation in [state], and add the corresponding index equalities.
