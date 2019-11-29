squirrel-prover/squirrel-prover

commit aa8ef73e3c7ed85d001bc8a74109f4b405e507e0

More scalable tactic syntax trees
Introduce a notion of tactic AST with simple named tactics at
their leaves. This is what the parser produces, and what is
pretty-printed: these parts of the code are now independent of
the list of actually declared tactics. To make this possible
I introduced a generic notion of tactic argument.

Prover.Prover_tactics is now the single place where a new
tactic should be declared. In the future, this declaration will
come with documentation for the user.

The tactics ASTs are actually independent of the judgment type:
we have generics ASTs in Tactics, and they are instantiated in
Prover.AST for Judgment.t tactics.

The type of tactics has also been simplified with a purely
abstract return type. This is a stronger type discipline than
the former polymorphic type, which also could not be maintained
when storing tactics in the Prover_tactics hash table.

All these changes should have no effect on the user interface.
There is a minor exception that I have no investigated in detail,
in "tests/ok/imply.mbc".

src/prover.ml
@@ -60,186 +60,179 @@ let rec reset_state n =
  *   general treatment difficult
  *   solution: a general notion of tactic args and associated syntax ? *)
 
-type tac =
-  | Admit : tac
-  | Ident : tac
-
-  | NoSimp : tac -> tac
-  | Left : tac
-  | Right : tac
-  | Intro : tac
-  | Split : tac
-
-  | Apply : (string * subst) -> tac
-  | Assert : Formula.formula -> tac
-
-  | ForallIntro : tac
-  | ExistsIntro : subst -> tac
-  | AnyIntro : tac
-
-  | GammaAbsurd : tac
-  | ConstrAbsurd : tac
-
-  | Assumption : tac
-
-  | EqNames : tac
-  | EqTimestamps : tac
-  | EqConstants : fname -> tac
-
-  (* | UProveAll : utac -> utac *)
-  | AndThen : tac * tac -> tac
-  | OrElse : tac * tac -> tac
-  | Try : tac -> tac
-  | Repeat : tac -> tac
-
-  | Euf : int -> tac
-  | CollisionResistance : tac
-  | Cycle : int -> tac
-
-let rec pp_tac : Format.formatter -> tac -> unit =
-  fun ppf tac -> match tac with
-    | Admit -> Fmt.pf ppf "admit"
-    | Ident -> Fmt.pf ppf "ident"
-    | NoSimp t -> Fmt.pf ppf "no simplification for %a" pp_tac t
-    | Left -> Fmt.pf ppf "goal_or_intro_l"
-    | Right -> Fmt.pf ppf "goal_or_intro_r"
-    | Intro -> Fmt.pf ppf "goal_intro"
-    | Split -> Fmt.pf ppf "goal_and_intro"
-
-    | Apply (s, t) ->
-        if t = [] then
-          Fmt.pf ppf "apply %s" s
-        else
-          Fmt.pf ppf "apply %s to .." s
-    | Assert f ->
-        Fmt.pf ppf "assert %a" Formula.pp_formula f
-
-    | ForallIntro -> Fmt.pf ppf "forall_intro"
-    | ExistsIntro (nu) ->
-      Fmt.pf ppf "@[<v 2>exists_intro@;%a@]"
-        pp_subst nu
-    | AnyIntro -> Fmt.pf ppf "any_intro"
-    | GammaAbsurd -> Fmt.pf ppf "gamma_absurd"
-    | ConstrAbsurd -> Fmt.pf ppf "constr_absurd"
-
-    | Assumption -> Fmt.pf ppf "assumption"
-
-    | EqNames -> Fmt.pf ppf "eq_names"
-    | EqTimestamps -> Fmt.pf ppf "eq_timestamps"
-    | EqConstants fn -> Fmt.pf ppf "eq_constants %a" pp_fname fn
-
-    (* | ProveAll utac -> Fmt.pf ppf "apply_all(@[%a@])" pp_tac utac *)
-    | AndThen (ut, ut') ->
-      Fmt.pf ppf "@[%a@]; @,@[%a@]" pp_tac ut pp_tac ut'
-    | OrElse (ut, ut') ->
-      Fmt.pf ppf "@[%a@] + @,@[%a@]" pp_tac ut pp_tac ut'
-    | Try ut ->
-      Fmt.pf ppf "try@[%a@]" pp_tac ut
-    | Repeat t ->
-      Fmt.pf ppf "repeat @[%a@]]" pp_tac t
-    (* | TacPrint ut -> Fmt.pf ppf "@[%a@].@;" pp_tac ut *)
-
-    | Euf i -> Fmt.pf ppf "euf %d" i
-    | CollisionResistance -> Fmt.pf ppf "collision resistance"
-    | Cycle i -> Fmt.pf ppf "cycle %d" i
-
-let rec tac_apply :
-  type a.
-  tac -> a Trace_tactics.tac
-=
-  let open Trace_tactics in
-  fun tac judge sk fk -> match tac with
-    | Admit -> sk [Judgment.set_formula True judge] fk
-    | Ident -> sk [judge] fk
-    | NoSimp tac -> tac_apply tac judge sk fk
-    | ForallIntro -> goal_forall_intro judge sk fk
-    | ExistsIntro (nu) -> goal_exists_intro nu judge sk fk
-    | AnyIntro -> goal_any_intro judge sk fk
-    | Apply (gname, s) ->
-        let f =
-          match
-            List.filter (fun (name,g) -> name = gname) !goals_proved
-          with
-            | [(_,f)] -> f
-            | [] -> raise @@ Failure "No proved goal with given name"
-            | _ -> raise @@ Failure "Multiple proved goals with the same name"
-        in
-        apply f s judge sk fk
-    | Assert f -> tac_assert f judge sk fk
-    | Left -> goal_or_intro_l judge sk fk
-    | Right -> goal_or_intro_r judge sk fk
-    | Split -> goal_and_intro judge sk fk
-    | Intro -> goal_intro judge sk fk
-
-    | GammaAbsurd -> gamma_absurd judge sk fk
-    | ConstrAbsurd -> constr_absurd judge sk fk
-
-    | Assumption -> assumption judge sk fk
-
-    | EqNames -> eq_names judge sk fk
-    | EqTimestamps -> eq_timestamps judge sk fk
-    | EqConstants fn -> eq_constants fn judge sk fk
-    | Euf i ->
-      let f_select _ t = t.id = i in
-      euf_apply f_select judge sk fk
-
-    (* | ProveAll tac -> prove_all judge (tac_apply gt tac) sk fk *)
-    | CollisionResistance -> collision_resistance judge sk fk
-    | AndThen (tac,tac') ->
-      Tactics.andthen
-        (tac_apply tac)
-        (tac_apply tac')
-        judge sk fk
-
-    | OrElse (tac,tac') ->
-      Tactics.orelse (tac_apply tac) (tac_apply tac') judge sk fk
-
-    (* Try is just syntactic sugar *)
-    | Try tac -> tac_apply (OrElse(tac,Ident)) judge sk fk
-
-    | Repeat tac ->
-      Tactics.repeat (tac_apply tac) judge sk fk
-    | Cycle _ -> assert false   (* This is not a focused tactic. *)
-
-    (* | TacPrint tac ->
-     *   Fmt.pr "%a @[<hov 0>%a@]@;%!"
-     *     (Fmt.styled `Bold ident) "Tactic:" pp_tac tac;
-     *   tac_apply gt tac judge (fun judge fk ->
-     *       Fmt.pr "%a%!" (Judgment.pp_judgment (pp_gt_el gt)) judge;
-     *       sk judge fk)
-     *     fk *)
-
-let simpGoal =
-  AndThen(Repeat AnyIntro,
-          AndThen(EqNames,
-                  AndThen(EqTimestamps,
-                          AndThen(Try GammaAbsurd,
-                                  Try ConstrAbsurd))))
+(* Tactic arguments. The presence of substitution is a bit ad-hoc,
+ * as visible in the parser: TODO we should probably just take a list
+ * of terms and let the tactic process it. *)
+type tac_arg =
+  | Subst of subst
+  | Goal_name of string
+  | Formula of Formula.formula
+  | Function_name of fname
+  | Int of int
 
 exception Tactic_Soft_Failure of string
 
+(** In the future this will have to be made generic since we will
+  * want the same declaration system for indistinguishability tactics. *)
+module rec Prover_tactics : sig
+
+  type tac = Judgment.t Tactics.tac
+
+  val register_general : string -> (tac_arg list -> tac) -> unit
+  val register : string -> tac -> unit
+  val register_subst : string -> (subst -> tac) -> unit
+  val register_int : string -> (int -> tac) -> unit
+  val register_formula : string -> (formula -> tac) -> unit
+  val register_fname : string -> (fname -> tac) -> unit
+  val register_macro : string -> AST.t -> unit
+
+  val get : string -> tac_arg list -> tac
+
+end = struct
+
+  type tac = Judgment.t Tactics.tac
+
+  let table :
+    (string, tac_arg list -> tac) Hashtbl.t =
+    Hashtbl.create 97
+
+  let get id =
+    try Hashtbl.find table id with
+      | Not_found -> failwith (Printf.sprintf "unknown tactic %S" id)
+
+  let register_general id f =
+    assert (not (Hashtbl.mem table id)) ;
+    Hashtbl.add table id f
+
+  let register id f =
+    register_general id
+      (fun args j sk fk ->
+         if args = [] then f j sk fk else
+           raise @@
+           Tactics.Tactic_Hard_Failure "this tactic does not take arguments")
+
+  let register_int id f =
+    register_general id
+      (fun args j sk fk -> match args with
+         | [Int x] -> f x j sk fk
+         | _ ->
+             raise @@
+             Tactics.Tactic_Hard_Failure "int argument expected")
+
+  let register_formula id f =
+    register_general id
+      (fun args j sk fk -> match args with
+         | [Formula x] -> f x j sk fk
+         | _ ->
+             raise @@
+             Tactics.Tactic_Hard_Failure "formula argument expected")
+
+  let register_fname id f =
+    register_general id
+      (fun args j sk fk -> match args with
+         | [Function_name x] -> f x j sk fk
+         | _ ->
+             raise @@
+             Tactics.Tactic_Hard_Failure "function name argument expected")
+
+  let register_subst id f =
+    register_general id
+      (fun args j sk fk -> match args with
+         | [Subst x] -> f x j sk fk
+         | _ ->
+             raise @@
+             Tactics.Tactic_Hard_Failure "illegal arguments")
+
+  let register_macro id m = Prover_tactics.register id (AST.eval m)
+
+end
+
+and AST :
+  Tactics.AST_sig with type arg = tac_arg with type judgment = Judgment.t
+= Tactics.AST(struct
+
+  type arg = tac_arg
+
+  type judgment = Logic.Judgment.judgment
+
+  let pp_arg ppf = function
+    | Int i -> Fmt.int ppf i
+    | Goal_name s -> Fmt.string ppf s
+    | Function_name fname -> pp_fname ppf fname
+    | Formula formula -> pp_formula ppf formula
+    | Subst subst -> pp_subst ppf subst
+
+  let eval_abstract id args : judgment Tactics.tac =
+    Prover_tactics.get id args
+
+  let pp_abstract ~pp_args s args ppf =
+    match s,args with
+      | "apply",[Goal_name id] ->
+          Fmt.pf ppf "apply %s" id
+      | "apply",Goal_name id::l ->
+          Fmt.pf ppf "apply %s to %a" id pp_args l
+      | _ -> raise Not_found
+
+end)
+
+let () =
+  Prover_tactics.register "admit" (fun j sk fk -> sk [] fk) ;
+  Prover_tactics.register "id" Tactics.id
+
+exception Return of Judgment.t list
+
 (** The evaluation of a tactic, may either raise a soft failure or a hard
-    failure (cf tactics.ml). A soft failure should be formatted inside the
-    Tactic_soft_failure exception.
-    A hard failure inside Tactic_hard_failure. Those exceptions are catched
-    inside the interactive loop. *)
-let eval_tactic_judge : tac -> Judgment.t -> Judgment.t list = fun tac judge ->
-  (* the failure should raise the soft failure, according to [pp_tac_error] *)
-  let failure_k tac_error =
-    raise @@ Tactic_Soft_Failure (Fmt.strf "%a" Tactics.pp_tac_error tac_error )
+  * failure (cf tactics.ml). A soft failure should be formatted inside the
+  * [Tactic_Soft_Failure] exception.
+  * A hard failure inside Tactic_hard_failure. Those exceptions are caught
+  * inside the interactive loop.
+  *
+  * TODO update and clarify this, the soft failure does not belong to
+  * Tactics *)
+let eval_tactic_judge ast j =
+  let tac = AST.eval ast in
+  (* The failure should raise the soft failure,
+   * according to [pp_tac_error]. *)
+  let fk tac_error =
+    raise @@
+    Tactic_Soft_Failure (Fmt.strf "%a" Tactics.pp_tac_error tac_error)
   in
-  let suc_k judges _ =
+  let sk l _ = raise (Return l) in
+  try ignore (tac j sk fk) ; assert false with Return l -> l
+
+(** Automatic simplification of generated subgoals *)
+
+let simpGoal =
+  AST.(
+    AndThen [
+      Repeat (Abstract ("anyintro",[])) ;
+      Abstract ("eqnames",[]) ;
+      Abstract ("eqtimestamps",[]) ;
+      Try (Abstract ("congruence",[])) ;
+      Try (Abstract ("notraces",[])) ])
+
+let remove_finished judges =
+  List.filter
+    (fun j ->
+       match j.Judgment.formula with
+       | True -> false
+       | _ -> true)
     judges
-  in
-     tac_apply tac judge suc_k failure_k
+
+let simplify j =
+  match j.Judgment.formula with
+  | Exists (vs,p) when vs = [] ->
+    Judgment.set_formula (p) j
+  (* TODO add more ? *)
+  | _ -> j
 
 let auto_simp judges =
-  List.map Trace_tactics.simplify judges
-  |> Trace_tactics.remove_finished
+  List.map simplify judges
+  |> remove_finished
   |> List.map (eval_tactic_judge simpGoal)
   |> List.flatten
-  |> List.map Trace_tactics.simplify
-  |> Trace_tactics.remove_finished
+  |> List.map simplify
+  |> remove_finished
 
 let tsubst_of_judgment j =
   List.map
@@ -310,10 +303,15 @@ let parse_args_exists ts : subst =
     | _ -> raise @@ Failure "Cannot parse term for existential intro which does
 not exists"
 
+let get_goal_formula gname =
+  match
+    List.filter (fun (name,g) -> name = gname) !goals_proved
+  with
+    | [(_,f)] -> f
+    | [] -> failwith "No proved goal with given name"
+    | _ -> failwith "Multiple proved goals with the same name"
 
 (** Declare Goals And Proofs *)
-
-type args = (string * Theory.kind) list
 
 let make_goal f  =
   (* In the rest of this function, the lists need to be reversed and appended
@@ -330,7 +328,7 @@ let make_goal f  =
 type parsed_input =
   | ParsedInputDescr
   | ParsedQed
-  | ParsedTactic of tac
+  | ParsedTactic of AST.t
   | ParsedUndo of int
   | ParsedGoal of gm_input
   | EOF
@@ -361,8 +359,9 @@ let complete_proof () =
     add_proved_goal (Utils.opt_get !current_goal);
     current_goal := None;
     subgoals := []
-  with Not_found ->  raise @@ Tactics.Tactic_Hard_Failure "Cannot complete proof
-with empty current goal"
+  with Not_found ->
+    raise @@ Tactics.Tactic_Hard_Failure "Cannot complete proof \
+               with empty current goal"
 
 let pp_goal ppf () = match !current_goal, !subgoals with
   | None,[] -> assert false
@@ -375,15 +374,16 @@ let pp_goal ppf () = match !current_goal, !subgoals with
 
 (** [eval_tactic_focus tac] applies [tac] to the focused goal.
   * @return [true] if there are no subgoals remaining. *)
-let eval_tactic_focus : tac -> bool = fun tac -> match !subgoals with
+let eval_tactic_focus tac = match !subgoals with
   | [] -> assert false
   | judge :: ejs' ->
-    let new_j =  (eval_tactic_judge tac judge) in
-    let ejs =  (match tac with
-      | NoSimp t -> new_j
-      | _ -> auto_simp new_j) @ ejs' in
-      subgoals := ejs;
-      is_proof_completed ()
+    let new_j = eval_tactic_judge tac judge in
+    let new_j = match tac with
+      | AST.Modifier ("nosimpl",t) -> new_j
+      | _ -> auto_simp new_j
+    in
+    subgoals := new_j @ ejs';
+    is_proof_completed ()
 
 let cycle i l =
   let rec cyc acc i = function
@@ -395,10 +395,9 @@ let cycle i l =
   if i < 0 then cyc [] (List.length l + i) l
   else cyc [] i l
 
-let eval_tactic : tac -> bool = fun utac -> match utac with
-  | Cycle i -> subgoals := cycle i !subgoals; false
+let eval_tactic utac = match utac with
+  | AST.Abstract ("cycle",[Int i]) -> subgoals := cycle i !subgoals; false
   | _ -> eval_tactic_focus utac
-
 
 let start_proof () = match !current_goal, !goals with
   | None, (gname,goal) :: _ ->

src/trace_tactics.ml
@@ -3,26 +3,11 @@ open Logic
 open Formula
 open Term
 
-type 'a tac = ('a,Judgment.t) Tactics.tac
+type tac = Judgment.t Tactics.tac
 
-(** Simplification utilities *)
+module T = Prover.Prover_tactics
 
-let remove_finished judges =
-  List.filter
-    (fun j ->
-       match j.Judgment.formula with
-       | True -> false
-       | _ -> true)
-    judges
-
-let simplify j =
-  match j.Judgment.formula with
-  | Exists (vs,p) when vs = [] ->
-    Judgment.set_formula (p) j
-  (* TODO add more ? *)
-  | _ -> j
-
-(** Introduction Rules *)
+(** Propositional connectives *)
 
 let goal_or_intro_l (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
@@ -36,10 +21,10 @@ let goal_or_intro_r (judge : Judgment.t) sk fk =
   | (Or (_, rformula)) -> sk [Judgment.set_formula (rformula) judge] fk
   | _ -> fk (Tactics.Failure "Cannot introduce a disjunction")
 
-(** To prove phi \/ psi, try first to prove phi and then psi *)
-let goal_or_intro (judge : Judgment.t) sk fk =
-  Tactics.orelse goal_or_intro_l goal_or_intro_r judge sk fk
+let () = T.register "left" goal_or_intro_l
+let () = T.register "right" goal_or_intro_r
 
+(* TODO currently unused: remove? *)
 let goal_true_intro (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
   | True -> sk () fk
@@ -52,6 +37,12 @@ let goal_and_intro (judge : Judgment.t) sk fk =
          Judgment.set_formula (rformula) judge ] fk
   | _ -> fk (Tactics.Failure "Cannot introduce a conjonction")
 
+let () = T.register "split" goal_and_intro
+
+(** Introduce various connectives to the right: currently,
+  * disjunction and implication (with conjunction on its left).
+  * TODO this is a bit arbitrary, and it will be surprising for
+  * users that "intro" does not introduce universal quantifiers. *)
 let goal_intro (judge : Judgment.t) sk fk =
   let exception No_intro in
   try
@@ -78,6 +69,10 @@ let goal_intro (judge : Judgment.t) sk fk =
                          or the left hand-side of an implication which \
                          is a conjonction")
 
+let () = T.register "intro" goal_intro
+
+(** Quantifiers *)
+
 (** Introduce the universally quantified variables and the goal. *)
 let goal_forall_intro (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
@@ -93,10 +88,11 @@ let goal_forall_intro (judge : Judgment.t) sk fk =
     let new_formula = subst_formula subst f in
     let new_judge = Judgment.set_formula new_formula judge
                     |> Judgment.set_env (!env)
-                    |> simplify
     in
     sk [new_judge] fk
   | _ -> fk (Tactics.Failure "Cannot introduce a forall")
+
+let () = T.register "forall_r" goal_forall_intro
 
 let goal_exists_intro nu (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
@@ -105,12 +101,31 @@ let goal_exists_intro nu (judge : Judgment.t) sk fk =
     sk [Judgment.set_formula new_formula judge] fk
   | _ -> fk (Tactics.Failure "Cannot introduce an exists")
 
+let () = T.register_subst "exists" goal_exists_intro
 
 let goal_any_intro j sk fk =
   Tactics.orelse_list
     [goal_and_intro;
      goal_intro;
      goal_forall_intro] j sk fk
+
+let () =
+  T.register_macro "intros"
+    Prover.AST.(
+      Repeat
+        (OrElse
+           [ Abstract ("intro",[]) ;
+             Abstract ("forall_r",[]) ]))
+
+let () =
+  T.register_macro "anyintro"
+    Prover.AST.(
+      OrElse
+        [ Abstract ("split",[]) ;
+          Abstract ("intro",[]) ;
+          Abstract ("forall_r",[]) ])
+
+(** Absurd *)
 
 let constr_absurd (judge : Judgment.t) sk fk =
   if not @@ Theta.is_sat judge.Judgment.theta then
@@ -122,6 +137,10 @@ let gamma_absurd (judge : Judgment.t) sk fk =
     sk [] fk
   else fk (Tactics.Failure "Equations satisfiable")
 
+let () = T.register "congruence" gamma_absurd
+
+let () = T.register "notraces" constr_absurd
+
 let assumption (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
   | True -> sk [] fk
@@ -130,6 +149,8 @@ let assumption (judge : Judgment.t) sk fk =
       sk [] fk
     else fk (Tactics.Failure "Not in hypothesis")
   | _ -> fk (Tactics.Failure "Not in hypothesis")
+
+let () = T.register "assumption" assumption
 
 (** Utils *)
 
@@ -151,7 +172,6 @@ let mk_and_cnstr l = match l with
       | x :: l -> mk_c (Bformula.And (x,acc)) l in
     mk_c a l'
 
-
 open Bformula
 
 (** Eq-Indep Axioms *)
@@ -170,6 +190,8 @@ let eq_names (judge : Judgment.t) sk fk =
   in
   sk [judge] fk
 
+let () = T.register "eqnames" eq_names
+
 let eq_constants fn (judge : Judgment.t) sk fk =
   let judge = Judgment.update_trs judge in
   let cnstrs =
@@ -182,6 +204,8 @@ let eq_constants fn (judge : Judgment.t) sk fk =
       ) judge cnstrs
   in
   sk [judge] fk
+
+let () = T.register_fname "eqconstants" eq_constants
 
 let eq_timestamps (judge : Judgment.t) sk fk =
   let ts_classes = Theta.get_equalities judge.Judgment.theta
@@ -213,6 +237,8 @@ let eq_timestamps (judge : Judgment.t) sk fk =
       judge facts
   in
   sk [judge] fk
+
+let () = T.register "eqtimestamps" eq_timestamps
 
 (** EUF Axioms *)
 
@@ -255,7 +281,6 @@ let euf_apply_schema theta (_, (_, key_is), m, s) case =
   in
   let constr = And (eq_cnstr, le_cnstr) in
   (new_f, constr)
-
 
 let euf_apply_direct theta (_, (_, key_is), m, _) dcase =
   let open Euf in
@@ -300,6 +325,12 @@ let euf_apply f_select (judge : Judgment.t) sk fk =
   let judge = Judgment.set_gamma g judge in
   (* TODO: need to handle failure somewhere. *)
   sk (euf_apply_facts judge at) fk
+
+let () =
+  T.register_int "euf"
+    (fun i ->
+       let f_select _ t = t.id = i in
+       euf_apply f_select)
 
 let apply gp (subst:subst) (judge : Judgment.t) sk fk =
   let exception No_apply in
@@ -361,6 +392,14 @@ let apply gp (subst:subst) (judge : Judgment.t) sk fk =
   with No_apply -> fk (Failure "Can only apply quantified conjunction of atoms
  or a disjunction implying a conjunction.")
 
+let () =
+  T.register_general "apply"
+    (function
+       | [Prover.Goal_name gname; Prover.Subst s] ->
+           let f = Prover.get_goal_formula gname in
+           apply f s
+       | _ -> raise @@ Tactics.Tactic_Hard_Failure "improper arguments")
+
 let tac_assert f j sk fk =
   let j1 = Judgment.set_formula f j in
   match Formula.formula_to_fact f with
@@ -369,6 +408,10 @@ let tac_assert f j sk fk =
         match Formula.formula_to_constr f with
           | constr -> sk [j1; Judgment.add_constr constr j] fk
           | exception Failure _ -> fk (Failure "unsupported formula")
+
+let () =
+  T.register_formula "assert"
+    (fun f j sk fk -> tac_assert f j sk fk)
 
 let collision_resistance (judge : Judgment.t) sk fk =
   let judge = Judgment.update_trs judge in
@@ -407,3 +450,5 @@ let collision_resistance (judge : Judgment.t) sk fk =
       ) judge new_facts
   in
   sk [judge] fk
+
+let () = T.register "collision" collision_resistance

commit 3cb85f79e66046fbc011eca60896b15c13ee4f3c

Simplify and generalize goal simplification
Add the introduction of true and empty existentials in the implicit
simplification tactic, which makes the simplify and remove_finished
steps superfluous.

Fix the problem on tests/ok/imply.mbc introduced in aa8ef73e:
in that commit I had removed a goal simplification performed at
the end of the intro tactic (it was rather ad-hoc, and created
a circular dependency between Prover and Trace_tactics); in the
test, an existential remained, was simplified, but because the
simplification was not iterated the goal could not be simplified
enough to be removed.

src/prover.ml
@@ -139,6 +139,7 @@ end = struct
     register_general id
       (fun args j sk fk -> match args with
          | [Subst x] -> f x j sk fk
+         | [] -> f [] j sk fk
          | _ ->
              raise @@
              Tactics.Tactic_Hard_Failure "illegal arguments")
@@ -202,7 +203,7 @@ let eval_tactic_judge ast j =
 
 (** Automatic simplification of generated subgoals *)
 
-let simpGoal =
+let simpl =
   AST.(
     AndThen [
       Repeat (Abstract ("anyintro",[])) ;
@@ -211,28 +212,10 @@ let simpGoal =
       Try (Abstract ("congruence",[])) ;
       Try (Abstract ("notraces",[])) ])
 
-let remove_finished judges =
-  List.filter
-    (fun j ->
-       match j.Judgment.formula with
-       | True -> false
-       | _ -> true)
-    judges
-
-let simplify j =
-  match j.Judgment.formula with
-  | Exists (vs,p) when vs = [] ->
-    Judgment.set_formula (p) j
-  (* TODO add more ? *)
-  | _ -> j
-
 let auto_simp judges =
-  List.map simplify judges
-  |> remove_finished
-  |> List.map (eval_tactic_judge simpGoal)
+  judges
+  |> List.map (eval_tactic_judge simpl)
   |> List.flatten
-  |> List.map simplify
-  |> remove_finished
 
 let tsubst_of_judgment j =
   List.map

src/trace_tactics.ml
@@ -24,11 +24,12 @@ let goal_or_intro_r (judge : Judgment.t) sk fk =
 let () = T.register "left" goal_or_intro_l
 let () = T.register "right" goal_or_intro_r
 
-(* TODO currently unused: remove? *)
 let goal_true_intro (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
-  | True -> sk () fk
+  | True -> sk [] fk
   | _ -> fk (Tactics.Failure "Cannot introduce true")
+
+let () = T.register "true" goal_true_intro
 
 let goal_and_intro (judge : Judgment.t) sk fk =
   match judge.Judgment.formula with
@@ -39,8 +40,7 @@ let goal_and_intro (judge : Judgment.t) sk fk =
 
 let () = T.register "split" goal_and_intro
 
-(** Introduce various connectives to the right: currently,
-  * disjunction and implication (with conjunction on its left).
+(** Introduce disjunction and implication (with conjunction on its left).
   * TODO this is a bit arbitrary, and it will be surprising for
   * users that "intro" does not introduce universal quantifiers. *)
 let goal_intro (judge : Judgment.t) sk fk =
@@ -51,8 +51,8 @@ let goal_intro (judge : Judgment.t) sk fk =
       | f when is_disjunction f ->
         let (f1,c1) = disjunction_to_atom_lists f in
         (List.map (fun c -> Bformula.Not c) f1,
-         List.map (fun c -> Bformula.Not c) c1)
-      , False
+         List.map (fun c -> Bformula.Not c) c1),
+        False
       | Impl(lhs,rhs) when is_conjunction lhs ->
         (conjunction_to_atom_lists lhs, rhs)
       | _ -> raise No_intro
@@ -103,27 +103,20 @@ let goal_exists_intro nu (judge : Judgment.t) sk fk =
 
 let () = T.register_subst "exists" goal_exists_intro
 
-let goal_any_intro j sk fk =
-  Tactics.orelse_list
-    [goal_and_intro;
-     goal_intro;
-     goal_forall_intro] j sk fk
-
 let () =
+  let open Prover.AST in
+  let non_branching_intro =
+    (* TODO neq *)
+    [ Abstract ("intro",[]) ;
+      Abstract ("exists",[]) ;
+      Abstract ("forall_r",[]) ;
+      Abstract ("true",[]) ]
+  in
   T.register_macro "intros"
-    Prover.AST.(
-      Repeat
-        (OrElse
-           [ Abstract ("intro",[]) ;
-             Abstract ("forall_r",[]) ]))
-
-let () =
+    (Repeat (OrElse non_branching_intro)) ;
   T.register_macro "anyintro"
-    Prover.AST.(
-      OrElse
-        [ Abstract ("split",[]) ;
-          Abstract ("intro",[]) ;
-          Abstract ("forall_r",[]) ])
+    (OrElse
+       (Abstract ("split",[]) :: non_branching_intro))
 
 (** Absurd *)
 

src/trace_tactics.mli
@@ -37,9 +37,6 @@ val goal_forall_intro : tac
     variables. *)
 val goal_exists_intro : Term.subst -> tac
 
-(** Syntactic sugar, trying to apply one of the intro. *)
-val goal_any_intro : tac
-
 (** [gamma_absurd judge sk fk] try to close the goal using congruence, else
     calls [fk] *)
 val gamma_absurd : tac

tests/ok/imply.mbc
@@ -1,5 +1,4 @@
 system null.
 goal test : forall (i:index), exists (), i=i => true.
 Proof.
-  intro.
 Qed.