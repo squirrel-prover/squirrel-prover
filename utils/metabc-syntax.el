36725bcdfad96ccd03f89e343bf771b3b6336a1f
Charlie Jacomme <jacomme@crans.org>
2020-05-14 14:36:04 +0200

    Update .el -> remove indentiation and update coloring

--- utils/metabc-syntax.el
+++ utils/metabc-syntax.el
@@ -1,10 +1,9 @@
-;;; metabc-syntax.el --- Syntax definitions for EasyCrypt mode
+;;; metabc-syntax.el --- Syntax definitions for MetaBC mode
 
 (require 'proof-syntax)
 
 (defvar metabc-prog-keywords '(
   "let"
-  "axiom"
   "in"
   "out"
   "if"
@@ -13,37 +12,108 @@
   "new"
   "try find"
   "such that"
+))
+
+(defvar metabc-decl-keywords '(
+  "axiom"
   "goal"
   "system"
   "Proof"
   "Qed"
   "hash"
+  "abstract"
   "name"
   "channel"
   "mutable"
-  "exists"
+  "term"
+  "equiv"
+  "process"
 ))
+
+
+
+
+(defvar metabc-fun-keywords '(
+  "input"
+  "cond"
+  "output"
+  "exec"
+  "frame"
+  "seq"
+  "diff"
+  ))
+
+(defvar metabc-type-keywords '(
+  "index"
+  "message"
+  "boolean"
+  "timestamp"
+  ))
+
 
 
 (defvar metabc-tactic-keywords '(
   "admit"
-  "split"
-  "left"
-  "right"
-  "forallintro"
-  "existsintro"
+  "anyintro"
+  "apply"
+  "assert"
+  "assumption"
+  "auto"
+  "case"
+  "collision"
   "congruence"
-  "notraces"
-  "euf"
-  "cycle"
-  "try"
-  "ident"
+  "constraints"
+  "depends"
   "eqnames"
+  "eqtraces"
+  "euf"
+  "executable"
+  "exists"
+  "existsleft"
+  "expand"
+  "false_left"
+  "fresh"
+  "forall"
+  "help"
+  "id"
+  "induction"
+  "intro"
+  "intros"
+  "introsleft"
+  "left"
+  "notleft"
+  "print"
+  "project"
+  "right"
+  "simpl"
+  "simpl_left"
+  "split"
+  "substitute"
+  "systemsubstitute"
+  "true"
+
+  "cca1"
+  "ddh"
+  "enckp"
+  "enrich"
+  "equivalent"
+  "expandall"
+  "fa"
+  "fadup"
+  "fresh"
+  "ifeq"
+  "noif"
+  "prf"
+  "refl"
+  "trivialif"
+  "xor"
+  "yesif"
 ))
 
 (defvar metabc-tacticals-keywords '(
   "try"
   "orelse"
+  "repeat"
 ))
 
 
@@ -69,6 +139,9 @@
     (cons (proof-ids-to-regexp metabc-tacticals-keywords)
                                                           'proof-tacticals-name-face)
     (cons (proof-ids-to-regexp metabc-tactic-keywords)    'proof-tactics-name-face)
+    (cons (proof-ids-to-regexp metabc-decl-keywords)    'font-lock-constant-face)
+    (cons (proof-ids-to-regexp metabc-fun-keywords)    'font-lock-preprocessor-face)
+    (cons (proof-ids-to-regexp metabc-type-keywords)    'font-lock-variable-name-face)
 	)
 )
 
