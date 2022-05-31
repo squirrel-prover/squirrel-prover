ec69049702add0137bab7ca4109d2d091df3be8d
Adrien Koutsos <adrien.koutsos@inria.fr>
2022-05-31 12:22:17 +0200

    fixed reduction issue w.r.t. systems
    changing the sequent system is unsound, as existing hypotheses may no
    longer apply to the new system

--- src/reduction.ml
+++ src/reduction.ml
@@ -30,179 +30,195 @@ module Mk (S : LowSequent.S) : S with type t := S.t = struct
      - trace literals not updated *)
   type state = { 
     table      : Symbols.table;
-    system     : SE.context;
+    sexpr      : SE.arbitrary;
     param      : red_param;
     hint_db    : Hint.hint_db;
     trace_lits : Term.literals;
     conds      : Term.term list;     (* accumulated conditions *)
   }
-                 
-  (** Reduce a term in a given context. 
-      The sequent's hypotheses must be used sparsingly *)
-  let reduce_term (param : red_param) (s : S.t) (t : Term.term) : Term.term = 
-    let exception NoExp in
-    
-    (* Invariant: we must ensure that fv(reduce(u)) ⊆ fv(t)
-       Return: reduced term, reduction occurred *)
-    (* FEATURE: memoisation *)
-    let rec reduce (st : state) (t : Term.term) : Term.term * bool = 
-      let t, has_red = reduce_head_once st t in
 
+  (** Internal *)
+  exception NoExp 
+
+  (* Invariant: we must ensure that fv(reduce(u)) ⊆ fv(t)
+     Return: reduced term, reduction occurred *)
+  (* FEATURE: memoisation *)
+  let rec reduce (st : state) (t : Term.term) : Term.term * bool = 
+    let t, has_red = reduce_head_once st t in
+
+    if has_red then fst (reduce st t), true
+    else
+      let t, has_red = reduce_subterms st t in
       if has_red then fst (reduce st t), true
-      else
-        let t, has_red = reduce_subterms st t in
-        if has_red then fst (reduce st t), true
-        else t, false
+      else t, false
 
-    (* Reduce once at head position *)
-    and reduce_head_once (st : state) (t : Term.term) : Term.term * bool = 
-      let t, has_red  = expand_head_once st t in
-      if has_red then t, true else rewrite_head_once st t 
+  (* Reduce once at head position *)
+  and reduce_head_once (st : state) (t : Term.term) : Term.term * bool = 
+    let t, has_red  = expand_head_once st t in
+    if has_red then t, true else rewrite_head_once st t 
 
-    (* Expand once at head position *)
-    and expand_head_once (st : state) (t : Term.term) : Term.term * bool = 
-      if not st.param.delta 
-      then t, false 
-      else try _expand_head_once st t with NoExp -> t, false
+  (* Expand once at head position *)
+  and expand_head_once (st : state) (t : Term.term) : Term.term * bool = 
+    if not st.param.delta 
+    then t, false 
+    else try _expand_head_once st t with NoExp -> t, false
 
-    and _expand_head_once (st : state) (t : Term.term) : Term.term * bool = 
-      let se = 
-        try SE.to_fset st.system.set 
-        with SE.Error _ -> raise NoExp (* nothing to expand if not a [fset] *)
-      in
-      match t with
-      | Term.Macro (ms, l, ts) ->
-        assert (l = []);
-        let models = 
-          match Constr.models_conjunct st.trace_lits with
-          | Utils.Timeout -> raise NoExp
-          | Utils.Result models -> models 
-        in 
-
-        if Constr.query ~precise:true models [`Pos, `Happens ts] then
-          match Macros.get_definition (S.mk_trace_cntxt s) ms ts with
-          | `Def mdef -> mdef, true
-          | _ -> raise NoExp
-        else raise NoExp
-
-      | Fun (fs, _, ts) 
-        when Operator.is_operator (S.table s) fs -> 
-        Operator.unfold (S.mk_trace_cntxt ~se s) fs ts, true
-
-      | _ -> raise NoExp
-
-    (* Rewrite once at head position *)
-    and rewrite_head_once (st : state) (t : Term.term) : Term.term * bool = 
-      let db = Hint.get_rewrite_db st.hint_db in
-      let hints = Match.Hm.find_dflt [] (Match.get_head t) db in
-
-      let rule = List.find_map (fun Hint.{ rule } ->
-          match Rewrite.rewrite_head st.table st.system.set rule t with
-          | None -> None
-          | Some (red_t, subs) ->
-            let subs_valid =  
-              List.for_all (fun sub -> 
-                  fst (reduce { st with param = rp_full } sub) = Term.mk_true
-                ) subs 
-            in              
-            if subs_valid then Some red_t else None            
-        ) hints
-      in
-
-      match rule with
-      | None -> t, false
-      | Some red_t -> red_t, true
-
-    (* Reduce all strict subterms *)
-    and reduce_subterms (st : state) (t : Term.term) : Term.term * bool = 
-      match t with
-      | Term.Exists (evs, t0) 
-      | Term.ForAll (evs, t0) -> 
-        let _, subst = Term.refresh_vars `Global evs in
-        let t0 = Term.subst subst t0 in
-        (* let st = { st with subst = subst @ st.subst; } in *)
-        let red_t0, has_red = reduce st t0 in
-
-        if not has_red then t, false
-        else
-          let r_subst = rev_subst subst in
-          let red_t0 = Term.subst r_subst red_t0 in
-          let red_t : Term.term = 
-            match t with
-            | Term.Exists _ -> Term.mk_exists ~simpl:false evs red_t0 
-            | Term.ForAll _ -> Term.mk_forall ~simpl:false evs red_t0 
-            | _ -> assert false
-          in
-          red_t, true
-
-      | Term.Seq (is, t0) ->
-        let _, subst = Term.refresh_vars `Global is in
-        let t0 = Term.subst subst t0 in
-        (* let st = { st with subst = subst @ st.subst; } in *)
-        let red_t0, has_red = reduce st t0 in
-
-        if not has_red then t, false
-        else
-          let r_subst = rev_subst subst in
-          let red_t0 = Term.subst r_subst red_t0 in
-          let red_t = Term.mk_seq0 is red_t0 in
-          red_t, true
-
-      | Term.Fun (fs, _, [c;t;e]) when fs = Term.f_ite -> 
-        let c, has_red0 = reduce st c in
-
-        let conds_t = c :: st.conds in
-        let conds_f = (Term.mk_not ~simpl:true c) :: st.conds in
-
-        let t, has_red1 = reduce { st with conds = conds_t } t in
-        let e, has_red2 = reduce { st with conds = conds_f } e in
-
-        Term.mk_ite ~simpl:false c t e,
-        has_red0 || has_red1 || has_red2
-
-      | Term.Find (is, c, t, e) -> 
-        let _, subst = Term.refresh_vars `Global is in
-        let c, t = Term.subst subst c, Term.subst subst t in
-        (* let st1 = { st with subst = subst @ st.subst; } in *)
-        let st1 = st in
-
-        let c, has_red0 = reduce st1 c in
-
-        let conds_t = c :: st.conds in
-        let conds_f = (Term.mk_not ~simpl:true c) :: st.conds in
-
-        let t, has_red1 = reduce { st1 with conds = conds_t } t in
-        let e, has_red2 = reduce { st  with conds = conds_f } e in
-
-        let r_subst = rev_subst subst in
-        let c, t = Term.subst r_subst c, Term.subst r_subst t in
-
-        Term.mk_find is c t e,
-        has_red0 || has_red1 || has_red2
-
-      | Term.Macro  _
-      | Term.Name   _
-      | Term.Fun    _
-      | Term.Action _
-      | Term.Var    _
-      | Term.Diff   _ -> 
-        let has_red, t = 
-          Term.tmap_fold (fun has_red t -> 
-              let t, has_red' = reduce st t in
-              has_red || has_red', t
-            ) false t
-        in
-        t, has_red
-
+  and _expand_head_once (st : state) (t : Term.term) : Term.term * bool = 
+    let se = 
+      try SE.to_fset st.sexpr
+      with SE.Error _ -> raise NoExp (* nothing to expand if not a [fset] *)
     in
-    let state = { 
-      table      = S.table s;
-      system     = S.system s;
+    let models = 
+      lazy (match Constr.models_conjunct st.trace_lits with
+          | Utils.Timeout -> raise NoExp
+          | Utils.Result models -> models)
+    in 
+    let cntxt () = Constr.{ 
+        table  = st.table; 
+        system = se; 
+        models = Some (Lazy.force models);
+      } in
+    match t with
+    | Term.Macro (ms, l, ts) ->
+      assert (l = []);
+
+      if Constr.query ~precise:true (Lazy.force models) [`Pos, `Happens ts] then
+        match Macros.get_definition (cntxt ()) ms ts with
+        | `Def mdef -> mdef, true
+        | _ -> raise NoExp
+      else raise NoExp
+
+    | Fun (fs, _, ts) 
+      when Operator.is_operator st.table fs -> 
+      Operator.unfold (cntxt ()) fs ts, true
+
+    | _ -> raise NoExp
+
+  (* Rewrite once at head position *)
+  and rewrite_head_once (st : state) (t : Term.term) : Term.term * bool = 
+    let db = Hint.get_rewrite_db st.hint_db in
+    let hints = Match.Hm.find_dflt [] (Match.get_head t) db in
+
+    let rule = List.find_map (fun Hint.{ rule } ->
+        match Rewrite.rewrite_head st.table st.sexpr rule t with
+        | None -> None
+        | Some (red_t, subs) ->
+          let subs_valid =  
+            List.for_all (fun sub -> 
+                fst (reduce { st with param = rp_full } sub) = Term.mk_true
+              ) subs 
+          in              
+          if subs_valid then Some red_t else None            
+      ) hints
+    in
+
+    match rule with
+    | None -> t, false
+    | Some red_t -> red_t, true
+
+  (* Reduce all strict subterms *)
+  and reduce_subterms (st : state) (t : Term.term) : Term.term * bool = 
+    match t with
+    | Term.Exists (evs, t0) 
+    | Term.ForAll (evs, t0) -> 
+      let _, subst = Term.refresh_vars `Global evs in
+      let t0 = Term.subst subst t0 in
+      (* let st = { st with subst = subst @ st.subst; } in *)
+      let red_t0, has_red = reduce st t0 in
+
+      if not has_red then t, false
+      else
+        let r_subst = rev_subst subst in
+        let red_t0 = Term.subst r_subst red_t0 in
+        let red_t : Term.term = 
+          match t with
+          | Term.Exists _ -> Term.mk_exists ~simpl:false evs red_t0 
+          | Term.ForAll _ -> Term.mk_forall ~simpl:false evs red_t0 
+          | _ -> assert false
+        in
+        red_t, true
+
+    | Term.Seq (is, t0) ->
+      let _, subst = Term.refresh_vars `Global is in
+      let t0 = Term.subst subst t0 in
+      (* let st = { st with subst = subst @ st.subst; } in *)
+      let red_t0, has_red = reduce st t0 in
+
+      if not has_red then t, false
+      else
+        let r_subst = rev_subst subst in
+        let red_t0 = Term.subst r_subst red_t0 in
+        let red_t = Term.mk_seq0 is red_t0 in
+        red_t, true
+
+    | Term.Fun (fs, _, [c;t;e]) when fs = Term.f_ite -> 
+      let c, has_red0 = reduce st c in
+
+      let conds_t = c :: st.conds in
+      let conds_f = (Term.mk_not ~simpl:true c) :: st.conds in
+
+      let t, has_red1 = reduce { st with conds = conds_t } t in
+      let e, has_red2 = reduce { st with conds = conds_f } e in
+
+      Term.mk_ite ~simpl:false c t e,
+      has_red0 || has_red1 || has_red2
+
+    | Term.Find (is, c, t, e) -> 
+      let _, subst = Term.refresh_vars `Global is in
+      let c, t = Term.subst subst c, Term.subst subst t in
+      (* let st1 = { st with subst = subst @ st.subst; } in *)
+      let st1 = st in
+
+      let c, has_red0 = reduce st1 c in
+
+      let conds_t = c :: st.conds in
+      let conds_f = (Term.mk_not ~simpl:true c) :: st.conds in
+
+      let t, has_red1 = reduce { st1 with conds = conds_t } t in
+      let e, has_red2 = reduce { st  with conds = conds_f } e in
+
+      let r_subst = rev_subst subst in
+      let c, t = Term.subst r_subst c, Term.subst r_subst t in
+
+      Term.mk_find is c t e,
+      has_red0 || has_red1 || has_red2
+
+    | Term.Diff (Explicit l) -> 
+      let has_red, l = 
+        List.map_fold (fun has_red (label,t) -> 
+            let st = { st with sexpr = SE.project [label] st.sexpr } in
+            let t, has_red' = reduce st t in
+            has_red || has_red', (label, t)
+          ) false l
+      in
+      Term.mk_diff l, has_red
+        
+    | Term.Macro  _
+    | Term.Name   _
+    | Term.Fun    _
+    | Term.Action _
+    | Term.Var    _ -> 
+      let has_red, t = 
+        Term.tmap_fold (fun has_red t -> 
+            let t, has_red' = reduce st t in
+            has_red || has_red', t
+          ) false t
+      in
+      t, has_red
+
+  (** [se] is the system of the term being reduced. *)
+  let mk_state ~se (param : red_param) (s : S.t) : state = 
+    { table      = S.table s;
+      sexpr      = se;
       param      = param;
       hint_db    = S.get_hint_db s;
       trace_lits = S.get_trace_literals s;
-      conds      = []; 
-    } in
+      conds      = []; } 
+
+  (** Reduce a term in a given context. 
+      The sequent's hypotheses must be used sparsingly *)
+  let reduce_term (param : red_param) (s : S.t) (t : Term.term) : Term.term = 
+    let state = mk_state ~se:(S.system s).set param s in
     let t, _ = reduce state t in
     t
 
@@ -234,12 +250,10 @@ module Mk (S : LowSequent.S) : S with type t := S.t = struct
 
     | Equiv.Atom (Equiv e) -> 
       let system = S.system s in
-      let new_system = SE.{ 
-          set = (oget system.pair :> SE.arbitrary); 
-          pair = None; 
-        } in
-      let s = S.set_system new_system s in
-      let e = List.map (reduce_term param s) e in
+      let e_se = (oget system.pair :> SE.arbitrary) in
+      let state = mk_state ~se:e_se param s in
+
+      let e = List.map (fst -| reduce state) e in
       Equiv.Atom (Equiv.Equiv e)
 
   (** We need type introspection there *)
