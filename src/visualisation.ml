d48508b0918938a7ca2d7db1ff109029295a44a5
Clément Hérouard <clement.herouard@irisa.fr>
2022-07-22 15:09:13 +0200

    Fix bug caused by visualisation on [any] system

--- src/visualisation.ml
+++ src/visualisation.ml
@@ -3,6 +3,7 @@ let dump () =
   let out_c = open_out "/tmp/dump.js" in
   let ppf = Format.formatter_of_out_channel out_c in
   match Prover.get_first_subgoal () with
-  | Trace j -> Constr.dump ppf (LowTraceSequent.mk_trace_cntxt j)
-  | Equiv j -> ();
+  | Trace j when SystemExpr.is_fset (TraceSequent.env j).system.set ->
+      Constr.dump ppf (TraceSequent.mk_trace_cntxt j)
+  | _ -> ();
   close_out out_c
