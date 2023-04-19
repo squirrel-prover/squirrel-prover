64da38adb752260a50866bde6f501446c5cd8e28
trubiano <thomas.rubiano@inria.fr>
2023-04-19 11:15:03 +0200

    Fix CI

--- jsexport.ml
+++ jsexport.ml
@@ -2,16 +2,11 @@ open Js_of_ocaml
 open Squirrellib
 
 let _ = 
+  (* Printer.init Printer.Html; *)
   Js.export "prover"
     (object%js
       method init = Prover.init ()
       method run f = Prover.run ~test:true f
       method exeCommand s st = Prover.exec_command s st
-      method execAll = Prover.exec_all
-    end)
-
-let _ = 
-  Js.export "main"
-    (object%js
-      method main () = Main.main ()
+      method execAll st s = Prover.exec_all st s
     end)
