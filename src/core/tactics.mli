c038bbec05730ee6a008f4748f753aa7e63601a8
Adrien Koutsos <adrien.koutsos@inria.fr>
2023-08-07 18:20:19 +0200

    cleanup tactics.ml

--- src/core/tactics.mli
+++ src/core/tactics.mli
@@ -8,27 +8,27 @@ type lsymb = string L.located
 (** {2 Tactics} *)
 
 (** A tactic ['a tac] is a non-deterministic computation
-  * transforming a goal of type ['a] into a list of subgoals.
-  *
-  * This non-deterministic computation is encoded by means of success and
-  * failure continuations. The success continuation takes as argument
-  * a possible result (a list of new subgoals) and a failure continuation
-  * that can be called to ask for other possible successes.
-
-  * As an example, if a tactic [tac] simply needs to change a goal
-  * [j] into a list of subgoals [l], [tac j sk fk] should simply be
-  * [sk l fk]. In particular, if [l] is empty, the initial goal will
-  * be considered proved.
-  *
-  * When a tactic cannot produce new results, it should call its failure
-  * continuation with an error explaining the failure. This is also the
-  * way to go when the tactic fails to apply.
-  *
-  * A tactic should raise an exception only if the tactic invocation
-  * is not well-formed. For instance, a typing error or a syntax error
-  * should raise an exception.
-  *
-  * We allow tactics to not make progress and not fail. *)
+    transforming a goal of type ['a] into a list of subgoals.
+    
+    This non-deterministic computation is encoded by means of success and
+    failure continuations. The success continuation takes as argument
+    a possible result (a list of new subgoals) and a failure continuation
+    that can be called to ask for other possible successes.
+   
+    As an example, if a tactic [tac] simply needs to change a goal
+    [j] into a list of subgoals [l], [tac j sk fk] should simply be
+    [sk l fk]. In particular, if [l] is empty, the initial goal will
+    be considered proved.
+   
+    When a tactic cannot produce new results, it should call its failure
+    continuation with an error explaining the failure. This is also the
+    way to go when the tactic fails to apply.
+   
+    A tactic should raise an exception only if the tactic invocation
+    is not well-formed. For instance, a typing error or a syntax error
+    should raise an exception.
+   
+    We allow tactics to not make progress and not fail. *)
 
 (*------------------------------------------------------------------*)
 (** Errors returned when the axiom syntactic side-conditions do not hold. *)
