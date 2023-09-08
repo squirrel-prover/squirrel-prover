72d0e5fa04830deb07d6e2885919b2cb1b41c6ef
David Baelde <baelde@lsv.fr>
2023-09-08 15:16:21 +0200

    Cleanup tests/ocaml/prover.ml

--- tests/ocaml/prover.ml
+++ tests/ocaml/prover.ml
@@ -8,28 +8,23 @@ module Theory = Squirrelcore.Theory
 
 (*------------------------------------------------------------------*)
 let some_print () =
-  let exception Ok in
-  Alcotest.check_raises "print stuff" Ok
-    (fun () ->
-      let st = TProver.init () in
-      let _ = try TProver.exec_all ~test:true st
-        "include Basic.
-        channel c
-        system [T] (S : !_i !_i new n; out(c,n)).
-        goal [T] foo (i:index) : happens(S(i,i)) => output@S(i,i) = n(i,i).
-        Proof.
-        admit.
-        Qed.
-        print n.
-        print cond.
-        print happens.
-        print coucou.
-        print foo.
-        name yo:message.
-        print yo."
-      with | e -> raise e in
-      raise Ok
-    )
+  let st = TProver.init () in
+  TProver.exec_all ~test:true st
+    "include Basic.\n\
+     channel c\n\
+     system [T] (S : !_i !_i new n; out(c,n)).\n\
+     goal [T] foo (i:index) : happens(S(i,i)) => output@S(i,i) = n(i,i).\n\
+     Proof.\n\
+       admit.\n\
+     Qed.\n\
+     print n.\n\
+     print cond.\n\
+     print happens.\n\
+     print coucou.\n\
+     print foo.\n\
+     name yo:message.\n\
+     print yo."
+  |> ignore
 
 let run_test () = 
   let exception Ok in
@@ -39,22 +34,8 @@ let run_test () =
        raise Ok
     )
 
-let compare_runner () = 
-  let t = Sys.time() in
-  let _ = TProver.run ~test:true "examples/lak-tags-full-wa.sp" in
-  let t1 = Sys.time() -. t in
-  Printf.printf "Execution time Prover: %fs\n" t1;
-  let t = Sys.time() in
-  let _ = Squirrellib.Main.run ~test:true "examples/lak-tags-full-wa.sp" in
-  let t2 = Sys.time() -. t in
-  Printf.printf "Execution time Main: %fs\n" t2;
-  Alcotest.(check' bool) ~msg:"Prover faster ?" 
-    ~actual:(t1<t2) ~expected:true;
-  Alcotest.(check' bool) ~msg:"Prover slower ?" 
-    ~actual:(t1>t2) ~expected:true
-
 (*------------------------------------------------------------------*)
-let tests = [ ("some_print",         `Quick, some_print);
-              ("run_test",           `Quick, run_test);
-              (* ("compare_runner",     `Quick, compare_runner); *)
-            ]
+let tests = [
+   ("some_print",         `Quick, some_print);
+   ("run_test",           `Quick, run_test);
+]
