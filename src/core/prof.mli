592665e8d3580e57c8502c1d15f2ccda4c3a240e
David Baelde <baelde@lsv.fr>
2024-01-02 06:43:34 +0100

    Missing signature from previous commit

--- src/core/prof.mli
+++ src/core/prof.mli
@@ -10,3 +10,6 @@ val print : Format.formatter -> unit -> unit
 val mk_unary   : string -> ('a -> 'b) -> 'a -> 'b
 val mk_binary  : string -> ('a -> 'b -> 'c) -> ('a -> 'b -> 'c)
 val mk_ternary : string -> ('a -> 'b -> 'c -> 'd) -> 'a -> 'b -> 'c -> 'd
+
+type profiler = { call : 'a. (unit -> 'a) -> 'a }
+val mk : string -> profiler
