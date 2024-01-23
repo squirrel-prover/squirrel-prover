2b8600bad97bf128784e5f09b4834ad5dd276878
David Baelde <david.baelde@irisa.fr>
2024-01-23 17:53:05 +0100

    Parse keywords as lsymbs in named args

---
+++ src/frontend/feedback.ml
@@ -0,0 +1,4 @@
+let keywords_as_ids = ref false
+let enable_keywords_as_ids () = keywords_as_ids := true
+let disable_keywords_as_ids () = keywords_as_ids := false
+let keywords_as_ids () = !keywords_as_ids
