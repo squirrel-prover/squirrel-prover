0639762d70ed7abf2122cfba04791bba0d839971
Clément Hérouard <clement.herouard@irisa.fr>
2024-08-16 02:38:56 +0200

    Comments - Add descriptions for each function in RwOracle.ml

--- src/tactics/rwOracle.ml
+++ src/tactics/rwOracle.ml
@@ -16,10 +16,14 @@ let soft_failure = Tactics.soft_failure
 let hard_failure = Tactics.hard_failure
 
 (*------------------------------------------------------------------*)
+(** The mode contains the information to modify the goal.
+    Allows factorasation of the code between equivalence goals and deduction goals. *)
 type mode = Equiv of Term.terms | Deduction of S.secrecy_goal
 
 
 (*------------------------------------------------------------------*)
+(** Get the mode from a sequent.
+    Raise a soft failure if the sequent is not an equivalence or deduction goal. *)
 let get_mode (s : ES.t) : mode =
   if S.conclusion_is_equiv s then
     Equiv (S.conclusion_as_equiv s)
@@ -38,11 +42,16 @@ let get_mode (s : ES.t) : mode =
     soft_failure (Tactics.GoalBadShape
       "expected an equivalence goal or a (non-)deduction goal with WeakSecrecy library")
 
+(** Get the list of terms contained in the goal. *)
 let get_terms (mode : mode) : Term.terms =
   match mode with
   | Equiv terms -> terms
   | Deduction goal -> goal.left
 
+(** Type the term given as argument to the tactic.
+    Raise a soft failure if the function does not have the type [ty1 -> ty2]
+    with both types encodable.
+    Return the typed term, [ty1] and [ty2] *)
 let convert_arg
     (term : Typing.term)
     (cenv : Typing.conv_env)
@@ -50,6 +59,7 @@ let convert_arg
   let t, ty = Typing.convert cenv term in
   match ty with
   | Type.(Fun (ty1, ty2)) ->
+    (** TODO : add support for oracles with multiple arguments *)
     if Type.is_bitstring_encodable ty1 && Type.is_bitstring_encodable ty2 then
       t, ty1, ty2
     else
@@ -57,6 +67,11 @@ let convert_arg
   | _ ->
     soft_failure (Failure "First argument must be a function")
 
+(** Return the terms of the goal that have to be rewritten.
+    Check that the type of this oracle is [ty]
+    Get the term in postion [i] if [pos = Some i].
+    Else, get the only oracle with the correct type or fails if there is not one possible choice.
+    Return the term of the oracle and its position. *)
 let get_oracle
     (pos : int L.located option)
     (ty : Type.ty)
@@ -80,6 +95,8 @@ let get_oracle
     | [oracle, i] -> oracle, i 
     | _ -> soft_failure (Failure "The goal contains several functions with the same type as the argument.\nPrecise the position of the oracle you want to rewrite.")
 
+(** Return a new sequent corresponding to [s]
+    with the oracle in position [i] replaced by [new_oracle]*)
 let mk_maingoal
     (oracle_new : Term.term)
     (mode : mode)
@@ -102,6 +119,10 @@ let mk_maingoal
     let conc = S.mk_secrecy_concl goal_new s in
     S.set_conclusion conc s
 
+(** Return a sequent for the side condition to prove to use [rewrite oracle].
+    Intuitively, the user has to prove that [oracle_old] and [oracle_new]
+    retuns the same result for any input computable with the terms contained
+    in [mode].*)
 let mk_subgoal
     (oracle_old : Term.term)
     (oracle_new : Term.term)
@@ -118,7 +139,6 @@ let mk_subgoal
   in
   let f_ty = Type.(Fun (ty, input_ty)) in
   let _, f_var = Vars.make_global `Approx (ES.vars s) f_ty "f" in
-
   let mk_term oracle =
     let term = 
       if List.length terms = 1 then
@@ -134,6 +154,7 @@ let mk_subgoal
                                 Atom (Reach loc_form))) in
   S.set_conclusion glob_form s
 
+(** Parse the argument of the tactic and return the two new sequent to prove.*)
 let rewrite_oracle_args (args : Args.parser_arg list) (s : ES.t) : ES.t list =
   match args with
   | [Args.RewriteOracle (term, pos_opt)] -> 
@@ -147,6 +168,7 @@ let rewrite_oracle_args (args : Args.parser_arg list) (s : ES.t) : ES.t list =
     [subgoal; maingoal]
   | _ -> hard_failure (Failure "improper arguments")
 
+(** Declare the tactic. *)
 let rewrite_oracle_tac args s sk fk =
   try sk (rewrite_oracle_args args s) fk with
   | Tactics.Tactic_soft_failure e -> fk e
