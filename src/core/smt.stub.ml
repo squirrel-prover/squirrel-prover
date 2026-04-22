335ffa17b2dfe4f66dc4c5270ee49fbf9710188a
Adrien Koutsos <adrien.koutsos@inria.fr>
2026-04-22 11:30:56 +0200

    fix(smt): add missing argument to stub smt implementation
    Fix #348

--- src/core/smt.stub.ml
+++ src/core/smt.stub.ml
@@ -1,6 +1,6 @@
 let[@warning "-27"] is_valid
   ~macro_axioms ~timeout ~steps ~provers ~cmd_flag
-  ~poly ~hint_tables env tbl system vars hyps hints concl
+  ~poly ~exact ~hint_tables env tbl system vars hyps hints concl
 =
   Format.eprintf "SMT support unavailable, please recompile with Why3.@.";
   false
